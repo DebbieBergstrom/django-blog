9412eed1476acefea1c1d71812302309cba61054
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-11-26 14:41:01 +0000

    Create comment model

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = (
+    (0, "Draft"), 
+    (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -11,7 +13,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
-    excerpts = models.TextField(blank=True)
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
@@ -28,4 +30,15 @@ class Post(models.Model):
 
 class Comment(models.Model):
     
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
     
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
